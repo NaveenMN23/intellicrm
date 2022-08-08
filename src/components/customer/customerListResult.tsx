74de90a6014ec964e9272e808ef9b6585a5143c8
NaveenMN23 <naveen23992@gmail.com>
2022-08-08 20:52:51 +0530

    WIP

---
+++ src/components/customer/customerListResult.tsx
@@ -0,0 +1,167 @@
+import { ChangeEvent, SetStateAction, JSXElementConstructor, Key, ReactElement, ReactFragment, ReactPortal, useState } from 'react';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+import PropTypes from 'prop-types';
+import {
+  Avatar,
+  Box,
+  Card,
+  Checkbox,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TablePagination,
+  TableRow,
+  Typography
+} from '@mui/material';
+import { getInitials } from './../../utils/get-initials';
+
+
+
+export const CustomerListResults = ({ customers, ...rest }:any) => {
+  const [selectedCustomerIds, setSelectedCustomerIds] = useState([]);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(0);
+
+  const handleSelectAll = (event: { target: { checked: any; }; }) => {
+    let newSelectedCustomerIds;
+
+    if (event.target.checked) {
+      newSelectedCustomerIds = customers.map((customer: { id: Number; }) => customer.id);
+    } else {
+      newSelectedCustomerIds = [];
+    }
+
+    setSelectedCustomerIds(newSelectedCustomerIds);
+  };
+
+  const handleSelectOne = (_: ChangeEvent<HTMLInputElement>, id: Number) => {
+    const selectedIndex = selectedCustomerIds.indexOf(id);
+    let newSelectedCustomerIds: Number[] = [];
+
+    if (selectedIndex === -1) {
+      newSelectedCustomerIds = newSelectedCustomerIds.concat(selectedCustomerIds, id);
+    } else if (selectedIndex === 0) {
+      newSelectedCustomerIds = newSelectedCustomerIds.concat(selectedCustomerIds.slice(1));
+    } else if (selectedIndex === selectedCustomerIds.length - 1) {
+      newSelectedCustomerIds = newSelectedCustomerIds.concat(selectedCustomerIds.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelectedCustomerIds = newSelectedCustomerIds.concat(
+        selectedCustomerIds.slice(0, selectedIndex),
+        selectedCustomerIds.slice(selectedIndex + 1)
+      );
+    }
+
+    setSelectedCustomerIds(newSelectedCustomerIds);
+  };
+
+  const handleLimitChange = (event: { target: { value: SetStateAction<number>; }; }) => {
+    setLimit(event.target.value);
+  };
+
+  const handlePageChange = (event: any, newPage: SetStateAction<number>) => {
+    setPage(newPage);
+  };
+
+  return (
+    <Card {...rest}>
+      <PerfectScrollbar>
+        <Box sx={{ minWidth: 1050 }}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell padding="checkbox">
+                  <Checkbox
+                    checked={selectedCustomerIds.length === customers.length}
+                    color="primary"
+                    indeterminate={
+                      selectedCustomerIds.length > 0
+                      && selectedCustomerIds.length < customers.length
+                    }
+                    onChange={handleSelectAll}
+                  />
+                </TableCell>
+                <TableCell>
+                  Name
+                </TableCell>
+                <TableCell>
+                  Email
+                </TableCell>
+                <TableCell>
+                  Location
+                </TableCell>
+                <TableCell>
+                  Phone
+                </TableCell>
+                <TableCell>
+                  Registration date
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {customers.slice(0, limit).map((customer: { id: Number; avatarUrl: string | undefined; name: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | null | undefined; email: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined; address: { city: any; state: any; country: any; }; phone: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined; }) => (
+                <TableRow
+                  hover
+                  key={customer.id}
+                  selected={selectedCustomerIds.indexOf(customer.id) !== -1}
+                >
+                  <TableCell padding="checkbox">
+                    <Checkbox
+                      checked={selectedCustomerIds.indexOf(customer.id) !== -1}
+                      onChange={(event) => handleSelectOne(event, customer.id)}
+                      value="true"
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <Box
+                      sx={{
+                        alignItems: 'center',
+                        display: 'flex'
+                      }}
+                    >
+                      <Avatar
+                        src={customer.avatarUrl}
+                        sx={{ mr: 2 }}
+                      >
+                        {getInitials(customer.name)}
+                      </Avatar>
+                      <Typography
+                        color="textPrimary"
+                        variant="body1"
+                      >
+                        {customer.name}
+                      </Typography>
+                    </Box>
+                  </TableCell>
+                  <TableCell>
+                    {customer.email}
+                  </TableCell>
+                  <TableCell>
+                    {`${customer.address.city}, ${customer.address.state}, ${customer.address.country}`}
+                  </TableCell>
+                  <TableCell>
+                    {customer.phone}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Box>
+      </PerfectScrollbar>
+      <TablePagination
+        component="div"
+        count={customers.length}
+        onPageChange={handlePageChange}
+        onRowsPerPageChange={handleLimitChange}
+        page={page}
+        rowsPerPage={limit}
+        rowsPerPageOptions={[5, 10, 25]}
+      />
+    </Card>
+  );
+};
+
+
+CustomerListResults.propTypes = {
+    customers: PropTypes.array.isRequired
+  };
---
+++ src/screens/customers/Customers.tsx
@@ -0,0 +1,32 @@
+import { Box, Container } from '@mui/material';
+import { CustomerListResults } from './../../components/customer/customerListResult';
+import { CustomerListToolbar } from './../../components/customer/customerListToolbar';
+// import { DashboardLayout } from '../components/dashboard-layout';
+import { customers } from './../../__mocks__/customers';
+
+const Customers = () => (
+  <>
+        Customers | Material Kit
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        py: 8
+      }}
+    >
+      <Container maxWidth={false}>
+        <CustomerListToolbar />
+        <Box sx={{ mt: 3 }}>
+          <CustomerListResults customers={customers} />
+        </Box>
+      </Container>
+    </Box>
+  </>
+);
+// Customers.getLayout = (page) => (
+//   <DashboardLayout>
+//     {page}
+//   </DashboardLayout>
+// );
+
+export default Customers;
