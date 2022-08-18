e6b027b80fb9b022953e1aff2798fd007aabcff5
NaveenMN23 <naveen23992@gmail.com>
2022-08-18 23:35:52 +0530

    Product fields added

--- src/screens/products/index.js
+++ src/screens/products/index.js
@@ -46,8 +46,18 @@ export default function Product(){
   const [rowData, setRowData] = useState(initialValues);
 
   const [columnDefs, setColumnDefs] = useState([
-    {field: 'productId'},
-    {field: 'productName'},
+    {field: 'productId', minWidth: 180},
+    {field: 'category', minWidth: 180},
+    {field: 'brandName', minWidth: 180},
+    {field: 'api', minWidth: 180},
+    {field: 'otherName', minWidth: 180},
+    {field: 'countryOfOrigin', minWidth: 180},
+    {field: 'manufacturer', minWidth: 180},
+    {field: 'dosage', minWidth: 180},
+    {field: 'qtyPerPack', minWidth: 95},
+    {field: 'dosageForm', minWidth: 180},
+    {field: 'strength', minWidth: 180},
+    {field: 'quantity', minWidth: 90}
   ]);
 
   const defaultColDef = useMemo(() => ({
@@ -84,7 +94,9 @@ export default function Product(){
   const onAddRow = useCallback((addIndex) => {
     console.log(gridRef.current.api);
     const res = gridRef.current.api.applyTransaction({
-      add: [{ productId: '', productName: '' }],
+      add: [{ productId: '', category: '', brandName:'', api:'', otherName:'',
+        countryOfOrigin:'', manufacturer:'', dosage:'', qtyPerPack:'', dosageForm:'',
+        strength:'', quantity:''}],
       addIndex: addIndex
       });
       console.log(res);
@@ -125,10 +137,28 @@ export default function Product(){
         console.log(err);
       } else {
         console.log("Rows uploaded:" + resp.rows);
+        // const obj = {
+        //   productId: '',
+        //   category: '',
+        //   brandName:'',
+        //   api:'',
+        //   otherName:'',
+        //   countryOfOrigin:'',
+        //   manufacturer:'',
+        //   dosage:'',
+        //   qtyPerPack:'',
+        //   dosageForm:'',
+        //   strength:'',
+        //   quantity:''
+        // }
         for(const el of resp.rows){
-          if(el[0].toString().toLowerCase().replace(/\s/g,'') !== "productid"){
-            tempUpdate.push({"productId": el[0], "productName": el[1]});
+          if(el[0] && el[0].toString().toLowerCase().replace(/\s/g,'') !== "productid"){
+            tempUpdate.push({"productId": el[0], "category": el[1], "brandName":el[2],
+              "api":el[3], "otherName":el[4], "countryOfOrigin":el[5], "manufacturer":el[6],
+              "dosage":el[7], "qtyPerPack":el[8], "dosageForm":el[9],
+              "strength":el[10], "quantity":el[11]});
             console.log("Rows uploaded:" + tempUpdate);
+
           }
         }
         setRowData({
@@ -165,10 +195,17 @@ export default function Product(){
       //   navigate('/customerlist')
       // }, 2000);
       const data = [
-        {productId: '1', productName: 'New'},
-        {productId: '2', productName: 'Existing'},
-        {productId: '3', productName: 'Latest'},
+        {productId: '1', category: 'item', brandName:'test', api:'123', otherName:'New',
+        countryOfOrigin:'IND', manufacturer:'Friek', dosage:'1', qtyPerPack:'12', dosageForm:'New',
+        strength:'120%', quantity:'3'},
+        {productId: '2', category: 'cat', brandName:'temp', api:'145', otherName:'Current',
+        countryOfOrigin:'USA', manufacturer:'Ford', dosage:'2', qtyPerPack:'3', dosageForm:'Two',
+        strength:'100%', quantity:'2'},
+        {productId: '3', category: 'glory', brandName:'it', api:'178', otherName:'Recent',
+        countryOfOrigin:'AUS', manufacturer:'Fiat', dosage:'1', qtyPerPack:'22', dosageForm:'',
+        strength:'150%', quantity:'5'},
       ];
+
       setRowData({
         ...rowData,
         "dataLoaded": true,
