a0f8f4683003cbfbd27b8efca9fcdc120dd83ef4
NaveenMN23 <naveen23992@gmail.com>
2022-09-05 21:40:38 +0530

    Delete user

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -6,12 +6,14 @@ export const EndPoints = {
     SAVE_CUSTOMER_DETAILS: 'SuperAdmin/CreateCustomer',
     SAVE_SUBADMIN_DETAILS: 'SuperAdmin/CreatSuperAdmin',
     GET_ALL_CUSTOMER_DETAILS: 'SuperAdmin/GetAllUserDetails/1',
-    GET_ALL_SUBADMIN_DETAILS: 'SuperAdmin/GetAllUserDetails/2',    
+    GET_ALL_SUBADMIN_DETAILS: 'SuperAdmin/GetAllUserDetails/2',
     GET_ALL_PRODUCT_DETAILS: 'Product/GetProductDetails',
     ADD_PRODUCT_DETAILS: 'Product/CreateProductDetails',
     GET_ALL_CUSTOMER_PRIORITY_DETAILS: 'SuperAdmin/GetAllUserPriority',
     UPDATE_CUSTOMER_PRIORITY_DETAILS: 'SuperAdmin/UpdateUserPriority',
-    GET_ALL_CUSTOMER_PRODUCT_DETAILS:"Product/GetCustomerProductDetails"
+    GET_ALL_CUSTOMER_PRODUCT_DETAILS:"Product/GetCustomerProductDetails",
+    DELETE_CUSTOMER: '',
+    DELETE_SUBADMIN: '',
 }
 
 /*
