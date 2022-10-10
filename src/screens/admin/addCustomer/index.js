4a56865fe5a19c784ae48e62e1196e96d3151cc2
NaveenMN23 <naveen23992@gmail.com>
2022-10-10 17:23:29 +0530

    mandatory symbol added

--- src/screens/admin/addCustomer/index.js
+++ src/screens/admin/addCustomer/index.js
@@ -283,7 +283,7 @@ function AddCustomer() {
                 onChange={handleInputChange} required="true" label="Credit Limit" variant="standard" fullWidth />
             </MDBox>
             <MDBox mb={2}>
-              <InputLabel sx={{lineHeight: '2.4375em'}}>Account Status</InputLabel>
+              <InputLabel sx={{lineHeight: '2.4375em'}}>Account Status *</InputLabel>
               <RadioGroup name="use-radio-group" defaultValue="first" onChange={handleCheckboxChange}>
                 {options.map((o) => (
                   <FormControlLabel value={o.id} label={o.key} key={o.key} control={<Radio required={true}/>} />
