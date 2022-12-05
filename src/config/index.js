7d1b1cc3c7c84c6fa85b4a719babd5712ff34636
Harikumarkite <63500433+Harikumarkite@users.noreply.github.com>
2022-12-05 08:51:48 +0530

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 /* Application config */
 export const appConfig = {
-    API_BASE_URL: "https://intellicrm-backend.herokuapp.com/api",
+    API_BASE_URL: "https://intellicrmservice-733u.onrender.com/api",
     API_VERSION: ""
 }
